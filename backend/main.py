83c40194c3ba69f5b3699f05e505c4c9bf9a2884
António Lopes <still_running_uphill@protonmail.com>
2025-05-20 23:41:58 +0100

    Added fastapi CRUD operations to main.py

---
+++ backend/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+#App object
+app = FastAPI()
+
+
+#CORS middleware
+origins = ["https://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#Routes
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/api/todo")
+async def get_todo():
+    return 1
+
+@app.get("/api/todo{id}")
+async def get_todo_by_id(id):
+    return 1
+
+@app.post("/api/todo")
+async def post_todo():
+    return 1
+
+@app.put("/api/todo{id}")
+async def put_todo(id, data):
+    return 1
+
+@app.delete("/api/todo{id}")
+async def delete_todo(id, data):
+    return 1
+
+
+if __name__ == "__main__":
+    main()
